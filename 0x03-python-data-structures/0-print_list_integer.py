89dd850dc31411dba893ea3de362f83bbab25696
= <=>
2022-07-02 22:14:49 +0300

    update answer

--- 0x03-python-data-structures/0-print_list_integer.py
+++ 0x03-python-data-structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
-#!/bin/python3
-
-
+#!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    
-    if my_list:
-        print('\n'.join(['{:d}'.format(n) for n in my_list]))
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
