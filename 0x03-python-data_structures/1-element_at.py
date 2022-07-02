c96c23e81c99abee701523bec688bc8a9f6641b5
= <=>
2022-07-02 22:37:36 +0300

    task 1 if statement reformat

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,10 +2,6 @@
 
 
 def element_at(my_list, idx):
-    if idx < 0:
-        return
-    elif idx >= len(my_list):
-        return
-    else: 
-        print(my_list[idx])
-
+    if my_list is not None and -1 < idx < len(my_list):
+        return my_list[idx]
+    return None
